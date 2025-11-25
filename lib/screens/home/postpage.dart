7bd5b1a80cb6543b830067fd50a201762a8d47a8
Nitesh Garg <niteshgarg.06@gmail.com>
2025-11-25 17:57:50 +0530

    Change selected tab to all

--- lib/screens/home/postpage.dart
+++ lib/screens/home/postpage.dart
@@ -12,7 +12,7 @@ class MyHome extends ConsumerStatefulWidget {
 }
 
 class _MyHomeState extends ConsumerState<MyHome> {
-  String selectedTab = 'Friends';
+  String selectedTab = 'All';
 
   String _getFilterForTab() {
     switch (selectedTab) {
@@ -21,9 +21,9 @@ class _MyHomeState extends ConsumerState<MyHome> {
       case 'Following':
         return 'following';
       case 'All':
-        return 'everyone';
+        return 'all';
       default:
-        return 'everyone';
+        return 'all';
     }
   }
 
